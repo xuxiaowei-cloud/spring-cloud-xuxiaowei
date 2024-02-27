b1b5e88909d0643e506e4c9b2060b4e756df8a45
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-02-27 21:57:26 +0800

    :art: spring-javaformat:apply

--- spring-cloud-xuxiaowei-gateway/src/main/java/cloud/xuxiaowei/gateway/SpringCloudXuxiaoweiGatewayApplication.java
+++ spring-cloud-xuxiaowei-gateway/src/main/java/cloud/xuxiaowei/gateway/SpringCloudXuxiaoweiGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCloudXuxiaoweiGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCloudXuxiaoweiGatewayApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudXuxiaoweiGatewayApplication.class, args);
+	}
 
 }
