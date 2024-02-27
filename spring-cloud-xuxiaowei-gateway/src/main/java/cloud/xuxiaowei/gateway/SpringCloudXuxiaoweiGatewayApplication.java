94a95b02f9ce6818a39be459abdf6137f81adfa2
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-02-27 21:47:21 +0800

    :tada: init dependencies/gateway

---
+++ spring-cloud-xuxiaowei-gateway/src/main/java/cloud/xuxiaowei/gateway/SpringCloudXuxiaoweiGatewayApplication.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudXuxiaoweiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudXuxiaoweiGatewayApplication.class, args);
+    }
+
+}
