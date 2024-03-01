0820966be0241c573b6c787a877848fc632f9a97
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-01 12:37:15 +0800

    :tada: init passport

---
+++ spring-cloud-xuxiaowei-passport/src/main/java/cloud/xuxiaowei/passport/SpringCloudXuxiaoweiPassportApplication.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.passport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudXuxiaoweiPassportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudXuxiaoweiPassportApplication.class, args);
+	}
+
+}
