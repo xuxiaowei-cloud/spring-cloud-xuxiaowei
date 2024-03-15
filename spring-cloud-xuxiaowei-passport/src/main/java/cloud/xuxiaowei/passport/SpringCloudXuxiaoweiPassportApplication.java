e3e1b06d663e43d0151aeabd3a5407b0caa9d56c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-15 16:01:12 +0800

    :sparkles: 使用注解：开启 OAuth 2.1 JDBC 实现
    为后期使用 OAuth 2.1 Redis 实现 做准备，可用于快速切换实现方式

--- spring-cloud-xuxiaowei-passport/src/main/java/cloud/xuxiaowei/passport/SpringCloudXuxiaoweiPassportApplication.java
+++ spring-cloud-xuxiaowei-passport/src/main/java/cloud/xuxiaowei/passport/SpringCloudXuxiaoweiPassportApplication.java
@@ -1,8 +1,16 @@
 package cloud.xuxiaowei.passport;
 
+import cloud.xuxiaowei.passport.annotation.EnableOAuth2JdbcAnnotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 项目启动入口
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@EnableOAuth2JdbcAnnotation
 @SpringBootApplication
 public class SpringCloudXuxiaoweiPassportApplication {
 
--- spring-cloud-xuxiaowei-passport/src/main/java/cloud/xuxiaowei/passport/config/AuthorizationServerConfig.java
+++ spring-cloud-xuxiaowei-passport/src/main/java/cloud/xuxiaowei/passport/config/AuthorizationServerConfig.java
@@ -26,15 +26,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
-import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
-import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
@@ -74,23 +67,6 @@ public class AuthorizationServerConfig {
 			);
 		// @formatter:on
 		return http.build();
-	}
-
-	@Bean
-	public RegisteredClientRepository registeredClientRepository(JdbcOperations jdbcOperations) {
-		return new JdbcRegisteredClientRepository(jdbcOperations);
-	}
-
-	@Bean
-	public OAuth2AuthorizationService authorizationService(JdbcOperations jdbcOperations,
-			RegisteredClientRepository registeredClientRepository) {
-		return new JdbcOAuth2AuthorizationService(jdbcOperations, registeredClientRepository);
-	}
-
-	@Bean
-	public OAuth2AuthorizationConsentService authorizationConsentService(JdbcOperations jdbcOperations,
-			RegisteredClientRepository registeredClientRepository) {
-		return new JdbcOAuth2AuthorizationConsentService(jdbcOperations, registeredClientRepository);
 	}
 
 	@Bean
