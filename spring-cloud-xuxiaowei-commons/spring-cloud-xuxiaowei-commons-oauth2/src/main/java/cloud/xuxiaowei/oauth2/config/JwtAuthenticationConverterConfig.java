2f4ec0e24f55031ab26e30a0169bdb7dcbf97477
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-20 20:07:07 +0800

    :sparkles: 使用 Redis 检查 JWT Token 是否存在于 Redis 中
    1. 使用 @EnableOAuth2TokenCheck 注解可一键开启配置
    2. 可使用配置文件自定义 Redis key 前缀

--- spring-cloud-xuxiaowei-commons/spring-cloud-xuxiaowei-commons-oauth2/src/main/java/cloud/xuxiaowei/oauth2/config/JwtAuthenticationConverterConfig.java
+++ spring-cloud-xuxiaowei-commons/spring-cloud-xuxiaowei-commons-oauth2/src/main/java/cloud/xuxiaowei/oauth2/config/JwtAuthenticationConverterConfig.java
@@ -1,6 +1,7 @@
 package cloud.xuxiaowei.oauth2.config;
 
 import cloud.xuxiaowei.oauth2.constant.OAuth2Constants;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -21,14 +22,19 @@ public class JwtAuthenticationConverterConfig {
 	 * 需要搭配 {@link EnableGlobalMethodSecurity#prePostEnabled} 设置为 {@link Boolean#TRUE} 使用
 	 */
 	@Bean
+	@ConditionalOnMissingBean(JwtAuthenticationConverter.class)
 	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+		return jwtConverter();
+	}
+
+	public static JwtAuthenticationConverter jwtConverter() {
 		JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
 
 		// 设置 Token 中获取权限数据的声明名称
 		grantedAuthoritiesConverter.setAuthoritiesClaimName(OAuth2Constants.AUTHORITIES);
 
 		// 设置成无前缀
-		grantedAuthoritiesConverter.setAuthorityPrefix("");
+		grantedAuthoritiesConverter.setAuthorityPrefix(OAuth2Constants.AUTHORITY_PREFIX);
 
 		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
 		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
