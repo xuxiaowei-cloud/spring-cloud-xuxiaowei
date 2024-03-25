461b9603b5101cd4ad5ddc386f08704c1d52939f
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-25 16:34:52 +0800

    :sparkles: OAuth 2.1 认证 异常处理

---
+++ spring-cloud-xuxiaowei-passport/src/main/java/cloud/xuxiaowei/passport/handler/ClientAuthenticationResponseHandler.java
@@ -0,0 +1,53 @@
+package cloud.xuxiaowei.passport.handler;
+
+import cloud.xuxiaowei.utils.Response;
+import cloud.xuxiaowei.utils.ResponseUtils;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+/**
+ * OAuth 2.1 认证 异常处理
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Slf4j
+public class ClientAuthenticationResponseHandler implements AuthenticationFailureHandler {
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+
+		log.error("OAuth 2.1 认证 异常处理：", exception);
+
+		Response<?> error = Response.error();
+
+		if (exception instanceof OAuth2AuthenticationException) {
+			OAuth2AuthenticationException e = (OAuth2AuthenticationException) exception;
+			OAuth2Error oauth2Error = e.getError();
+			String description = oauth2Error.getDescription();
+			String errorCode = oauth2Error.getErrorCode();
+			String uri = oauth2Error.getUri();
+
+			error.setCode(errorCode);
+			error.setMessage(description);
+			error.setUrl(uri);
+		}
+		else {
+			String message = "此异常暂未分类，请联系管理员";
+			error.setMessage(message);
+			log.error(message, exception);
+		}
+
+		ResponseUtils.response(response, error);
+	}
+
+}
