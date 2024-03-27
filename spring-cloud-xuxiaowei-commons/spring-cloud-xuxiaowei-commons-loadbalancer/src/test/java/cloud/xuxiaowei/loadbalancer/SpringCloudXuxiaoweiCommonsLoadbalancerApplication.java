3db76bf4a3f672046146c78870be4d3dcdcc4ae2
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-27 13:33:48 +0800

    :white_check_mark: 新增测试：RestTemplate LoadBalanced

---
+++ spring-cloud-xuxiaowei-commons/spring-cloud-xuxiaowei-commons-loadbalancer/src/test/java/cloud/xuxiaowei/loadbalancer/SpringCloudXuxiaoweiCommonsLoadbalancerApplication.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.loadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudXuxiaoweiCommonsLoadbalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudXuxiaoweiCommonsLoadbalancerApplication.class, args);
+	}
+
+}
