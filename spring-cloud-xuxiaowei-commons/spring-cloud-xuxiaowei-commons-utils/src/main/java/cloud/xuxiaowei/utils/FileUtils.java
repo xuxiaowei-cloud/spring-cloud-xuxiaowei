9f048cc004db87193957de1a3b12454fc35dfd3d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-27 23:15:44 +0800

    :sparkles: 读取文件内容

--- spring-cloud-xuxiaowei-commons/spring-cloud-xuxiaowei-commons-utils/src/main/java/cloud/xuxiaowei/utils/FileUtils.java
+++ spring-cloud-xuxiaowei-commons/spring-cloud-xuxiaowei-commons-utils/src/main/java/cloud/xuxiaowei/utils/FileUtils.java
@@ -1,8 +1,14 @@
 package cloud.xuxiaowei.utils;
 
 import cloud.xuxiaowei.utils.exception.CloudRuntimeException;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,6 +18,7 @@ import java.util.List;
  * @author xuxiaowei
  * @since 0.0.1
  */
+@Slf4j
 public class FileUtils {
 
 	/**
@@ -69,6 +76,81 @@ public class FileUtils {
 			throwable.setReferences(references);
 			throw throwable;
 		}
+	}
+
+	/**
+	 * 读取文件
+	 * @param filePath 文件路径
+	 * @return 返回 文件内容
+	 * @throws IOException 读取文件异常
+	 */
+	public static String readText(String filePath) throws IOException {
+		return readText(new File(filePath));
+	}
+
+	/**
+	 * 读取文件
+	 * @param file 文件
+	 * @return 返回 文件内容
+	 * @throws IOException 读取文件异常
+	 */
+	public static String readText(File file) throws IOException {
+		StringBuilder stringBuilder = new StringBuilder();
+		// 创建一个新FileReader，给定从中读取文件 。
+		try (FileReader fileReader = new FileReader(file);
+				// 创建一个使用默认大小输入缓冲器的缓冲字符输入流。
+				BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+			String readLine;
+			// 读取文本行。 的线被认为是由一个进料线中的任何一个被终止（“\n”），回车（“\r”），或回车立即由换行遵循。
+			while ((readLine = bufferedReader.readLine()) != null) {
+				stringBuilder.append(System.lineSeparator()).append(readLine);
+			}
+
+		}
+		catch (IOException e) {
+			log.error("读取文件异常：", e);
+			throw e;
+		}
+		return stringBuilder.toString();
+	}
+
+	/**
+	 * 读取文件
+	 * @param filePath 文件路径
+	 * @return 返回 文件内容
+	 * @throws IOException 读取文件异常
+	 */
+	public static List<String> readList(String filePath) throws IOException {
+		return readList(new File(filePath));
+	}
+
+	/**
+	 * 读取文件
+	 * @param file 文件
+	 * @return 返回 文件内容
+	 * @throws IOException 读取文件异常
+	 */
+	public static List<String> readList(File file) throws IOException {
+		List<String> list = new ArrayList<>();
+		// 创建一个新FileReader，给定从中读取文件 。
+		try (FileReader fileReader = new FileReader(file);
+
+				// 创建一个使用默认大小输入缓冲器的缓冲字符输入流。
+				BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+			String readLine;
+			// 读取文本行。 的线被认为是由一个进料线中的任何一个被终止（“\n”），回车（“\r”），或回车立即由换行遵循。
+			while ((readLine = bufferedReader.readLine()) != null) {
+				list.add(readLine);
+			}
+
+		}
+		catch (IOException e) {
+			log.error("读取文件异常：", e);
+			throw e;
+		}
+		return list;
 	}
 
 }
--- spring-cloud-xuxiaowei-commons/spring-cloud-xuxiaowei-commons-utils/src/test/java/cloud/xuxiaowei/utils/FileUtilsTests.java
+++ spring-cloud-xuxiaowei-commons/spring-cloud-xuxiaowei-commons-utils/src/test/java/cloud/xuxiaowei/utils/FileUtilsTests.java
@@ -2,12 +2,15 @@ package cloud.xuxiaowei.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author xuxiaowei
@@ -91,6 +94,50 @@ class FileUtilsTests {
 			tmp = e;
 		}
 		assertNotNull(tmp);
+	}
+
+	@Test
+	void readText() throws IOException {
+		String filePath = Paths.get("LICENSE").toAbsolutePath().toString();
+		String text = FileUtils.readText(filePath);
+		int length = text.length();
+		// Linux：11357
+		// Windows：11558
+		assertTrue(Arrays.asList(11558, 11357).contains(length));
+	}
+
+	@Test
+	void readTextError() {
+		String filePath = Paths.get(UUID.randomUUID().toString()).toAbsolutePath().toString();
+		String text = null;
+		try {
+			text = FileUtils.readText(filePath);
+		}
+		catch (Exception e) {
+			assertInstanceOf(FileNotFoundException.class, e);
+		}
+		assertNull(text);
+	}
+
+	@Test
+	void readList() throws IOException {
+		String filePath = Paths.get("LICENSE").toAbsolutePath().toString();
+		List<String> list = FileUtils.readList(filePath);
+		int size = list.size();
+		assertEquals(201, size);
+	}
+
+	@Test
+	void readListError() {
+		String filePath = Paths.get(UUID.randomUUID().toString()).toAbsolutePath().toString();
+		List<String> list = null;
+		try {
+			list = FileUtils.readList(filePath);
+		}
+		catch (Exception e) {
+			assertInstanceOf(FileNotFoundException.class, e);
+		}
+		assertNull(list);
 	}
 
 }
